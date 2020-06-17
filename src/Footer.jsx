a25227f8c76c24dc2339c2391ea6fa2c47b97141
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-17 16:46:53 +0200

    writing tests to ensure page renders correctly, adding footer.jsx and header.jsx files, defining the components in them, amending index.js referencing new components

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 id="footer">Made with React 16.12.0</h1>
+    </footer>
+  );
+};
+
+export default Footer;
