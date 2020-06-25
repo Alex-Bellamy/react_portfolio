936d0e70b2252d4f2f4b6c30658cf3eb65c7fdcc
Alex_Bellamy <alexlbellamy@gmail.com>
2020-06-25 20:13:03 +0200

    amending tests for home page wording and home page wording itself

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Hello = () => {
   return (
     <div className="ui main container">
-      <h1 id="hello">Hello World</h1>
+      <h1 id="hello">Alex_Bellamy.junior.fullStack.developer</h1>
+      <p>creative solutions delivered through <strong>test driven development </strong>& <strong>agile</strong> methodies</p>
     </div>
   );
 };
