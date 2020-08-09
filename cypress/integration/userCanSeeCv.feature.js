0cf8684415e687d23bcb1d6dc1152ee0fb3665f0
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-09 22:26:00 +0200

    amending user can see cv specs and adding additional tests

--- cypress/integration/userCanSeeCv.feature.js
+++ cypress/integration/userCanSeeCv.feature.js
@@ -1,22 +1,59 @@
-describe('User can see my CV', () => {
-   beforeEach(() => {
-       cy.visit('http://localhost:3003');
-       cy.get('#cv-tab').click();
-})
+describe("User can see my CV", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3003");
+    cy.get("#cv-tab").click();
+  });
 
-it ('displays my first job', () => {
-    cy.get('#cv-1').within(() => {
-        cy.get('.image').should('exist');
-        cy.get('.ui.header').should('contain', 'My First Job');
-        cy.get('.description').should('contain', 'This was my first job');
-    })
-}),
+  it("displays my latest role", () => {
+    cy.get("#cv-1").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should(
+        "contain",
+        "Hands-on full stack developer accelerated training (Sept 2020)"
+      );
+      cy.get(".description").should(
+        "contain",
+        "18 weeks mastering the skills to become a confident junior developer. A truly whirlwind experience of more than 600 hours of blood, sweet and nearly tears but absolutely worth it in the end"
+      );
+    });
+  }),
+  
+    it("displays my previous role", () => {
+      cy.get("#cv-2").within(() => {
+        cy.get(".image").should("exist");
+        cy.get(".ui.header").should(
+          "contain",
+          "Business advisory, recovery and bankruptcy advisory (2008 - 2020)"
+        );
+        cy.get(".description").should(
+          "contain",
+          "Advising and managing insolvent businesses and bankrtupt individuals. Professional client-facing role at three firms over the last twelve years. I have led complex projects involving global banks and coporations, adding value for all parties at every stage"
+        );
+      });
+    });
 
-it ('displays my qualifications', () => {
-    cy.get('#cv-2').within(() => {
-        cy.get('.image').should('exist');
-        cy.get('.ui.header').should('contain', 'My Qualifications');
-        cy.get('.description').should('contain', 'I have done quite a bit of learning over my lifetime...');
-    })
-});
+  it("displays my recovery qualification", () => {
+    cy.get("#cv-3").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should(
+        "contain",
+        "UK qualified konkursförvaltare (2015)"
+      );
+      cy.get(".description").should(
+        "contain",
+        "Personal Insolvency - Högskola type training"
+      );
+    });
+  });
+
+  it("displays my school qualification", () => {
+    cy.get("#cv-4").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "School education (age 14-18)");
+      cy.get(".description").should(
+        "contain",
+        "Three A Levels - information communication services, english language and geography. 10 GCSEs including mathematics and english"
+      );
+    });
+  });
 });
