df7d82c961608136625c1cbf021ba81e56d1f3db
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-09 23:25:08 +0200

    rearranging order in projects.json and updating user can see list of projects specs

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -1,30 +1,39 @@
-describe('User can see list of projects', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3003');
-      cy.get('#projects-tab').click();
-    })
-  
-    it('displays first project', () => {
-      cy.get('#project-1').within(() => {
-        cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', 'My First Website');
-        cy.get('.description').should('contain', 'This was my first project. The guy lost, but won the popular vote!');
-      })
-    });
-  
-    it('displays second project', () => {
-      cy.get('#project-2').within(() => {
-        cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', 'UI Design');
-        cy.get('.description').should('contain', 'Designing user interfaces is fun. I want to learn more about that...');
-      })
-    });
-  
-    it('displays third project', () => {
-      cy.get('#project-3').within(() => {
-        cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', 'Mobile UX');
-        cy.get('.description').should('contain', "I like to design for the mobile platform. The challenges to build UI's for smartphones is challenging but extremely rewarding.");
-      })
-    });  
+describe("User can see list of projects", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3003");
+    cy.get("#projects-tab").click();
   });
+
+  it("displays first project", () => {
+    cy.get("#project-1").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "My first website");
+      cy.get(".description").should(
+        "contain",
+        "The first website to make it out into the world when I started at Craft Academy"
+      );
+    });
+  });
+
+  it("displays second project", () => {
+    cy.get("#project-2").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "Fizz buzz challenge");
+      cy.get(".description").should(
+        "contain",
+        "A children's game and classic challenge harnessing my new Ruby skills"
+      );
+    });
+  });
+
+  it("displays third project", () => {
+    cy.get("#project-3").within(() => {
+      cy.get(".image").should("exist");
+      cy.get(".ui.header").should("contain", "Bmi calculator");
+      cy.get(".description").should(
+        "contain",
+        "For the fitness enthusiasts out there, this is first of two BMI calculators"
+      );
+    });
+  });
+});
